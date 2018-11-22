955ffaedd1e8d89238c641f2fdd21f9d301e4c83
Ehron Gwinn <ehron@local>
2018-11-22 08:19:25 -0500

    add install script and update readme

--- README.md
+++ README.md
@@ -4,6 +4,13 @@ This service returns a list of products retrieved from a JSON DB
 
 ## Instructions
 
+### Installation
+
+Git clone and run the install script.
+```
+cd install && sudo bash install.sh
+```
+
 ##### Start a local mongo instance for testing (if necessary):
 ```
 mkdir ~/product-data
@@ -21,3 +28,5 @@ pip install pymongo
 DB_ADDR='localhost' DB_PORT=27018 FLASK_APP=product.py \
 DB_NAME=bbthe90s COL_NAME=products PRODUCT_PORT=1234 flask run
 ```
+
+
---
+++ install/install.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# install the requirements
+sudo chown -R ubuntu:ubuntu /home/ubuntu/.cache
+pip3 install flask
+pip3 install pymongo
+
+# download the apply
+mkdir /home/ubuntu/src
+cd /home/ubuntu/src
+git clone https://github.com/norhe/product-service.git
+
+# systemd
+
+cat <<EOF | sudo tee /lib/systemd/system/product.service
+[Unit]
+Description=product.py - Listing service API
+After=network.target
+
+[Service]
+Type=simple
+User=ubuntu
+ExecStart=/usr/local/bin/envconsul -prefix product_conf /usr/bin/python3 /home/ubuntu/src/product-service/product.py
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable product.service
+sudo systemctl start product.service
